629de5dc0b939a53770a83dbe8e8062235c7d5d6
unknown <aguirreh8@gmail.com>
2018-05-12 20:11:20 -0400

    Add body parser dependency to fix undefined problem

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const nodemailer = require("nodemailer");
+const xoauth2 = require("xoauth2");
 const app = express();
+require("dotenv").config();
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("client/build"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const transporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',
+	type: 'OAuth2',
+	auth: 
+		{
+			type: 'OAuth2',
+			user: "aguirreh8@gmail.com",
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+		}
+	
+});
+
+app.post("/sendMail", function(req, res) {
+	const mailOptions = {
+		from: `${req.body.name} <${req.body.sender}>`,
+		to: "aguirreh8@gmail.com",
+		subject: req.body.subject,
+		text: `From: ${req.body.sender} \n ${req.body.message}`
+	}
+
+	transporter.sendMail(mailOptions, function(err, res) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			console.log("Email sent");
+		}
+	})
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
